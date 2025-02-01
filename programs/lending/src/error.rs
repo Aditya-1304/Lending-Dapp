e9afa6725a5f66fb60f40f84149f0064672518a3
Aditya Mandal <mandaladitya1614@gmail.com>
2025-02-01 11:27:29 +0530

    Added liquidate account indtructions

--- programs/lending/src/error.rs
+++ programs/lending/src/error.rs
@@ -10,4 +10,8 @@ pub enum ErrorCode{
   
   #[msg("Requested amount exceeds depositable amount")]
   OverRepay,
+
+  #[msg("User is not under collateralized, and cannot be liquidated")]
+  NotUnderCollateralized,
+
 }
